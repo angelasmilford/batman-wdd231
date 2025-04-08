bf7ad2da4c3c1c85820e8975262b1a44f55facf4
Hyrum Whittier <114303084+whittierht@users.noreply.github.com>
2025-04-08 09:22:17 -0600

    Update feedbackModal.js

--- scripts/feedbackModal.js
+++ scripts/feedbackModal.js
@@ -2,6 +2,7 @@ export function initFeedbackModal() {
     const modal = document.querySelector(".feedback-modal");
     const btn = document.querySelector(".btn-modal");
     const closebtn = document.querySelector(".close-btn");
+    
   
     if (!modal || !btn || !closebtn) return;
   
@@ -21,4 +22,22 @@ export function initFeedbackModal() {
         modal.style.display = "none";
       }
     };
+  }
+
+  export function initFormSubmit() {
+    const thanks = document.querySelector(".thank-you");
+    const modal = document.querySelector(".feedback-modal");
+    const form = document.querySelector(".feedback-form"); 
+  
+    if (!form || !thanks) return; 
+  
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      modal.style.display = 'none';
+      thanks.classList.add('show');
+  
+      setTimeout(() => {
+        thanks.classList.remove('show');
+      }, 3000);
+    });
   }
