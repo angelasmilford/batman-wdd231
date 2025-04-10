6226dc0323d1e805e59279542b6d022389adfff4
angelasmilford <angela.milford123@gmail.com>
2025-04-09 14:07:10 -1100

    deleting empty files and removing unnecessary whitespace for footer.css

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-import { toggleDarkMode } from "./darkmode.js";
-import { toggleMenu } from "./hamburger.js";
-import { toggleFeedbackModal } from "./feedbackModal.js";
-
-document.getElementById("darkModeToggle").addEventListener("click", toggleDarkMode);
-document.querySelector(".menu").addEventListener("click", toggleMenu); //change
-document.querySelector(".btn-modal").addEventListener("click", toggleFeedbackModal);
-document.querySelector(".close-btn").addEventListener("click", toggleFeedbackModal);
